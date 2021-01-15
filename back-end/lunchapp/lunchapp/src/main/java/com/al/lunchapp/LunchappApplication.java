fa599ca31beb908be6dc2b7dda0b96ebbc374f4e
MagiMarinova <magi.marinowax@gmail.com>
2021-01-15 15:29:57 +0200

    WebSocket Chat BE

--- back-end/lunchapp/lunchapp/src/main/java/com/al/lunchapp/LunchappApplication.java
+++ back-end/lunchapp/lunchapp/src/main/java/com/al/lunchapp/LunchappApplication.java
@@ -3,6 +3,9 @@ package com.al.lunchapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class LunchappApplication {
@@ -11,4 +14,13 @@ public class LunchappApplication {
 		SpringApplication.run(LunchappApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowCredentials(true).allowedOrigins("*").allowedMethods("*");
+			}
+		};
+	}
 }
---
+++ back-end/lunchapp/lunchapp/src/main/java/com/al/lunchapp/controllers/SocketController.java
@@ -0,0 +1,17 @@
+package com.al.lunchapp.controllers;
+
+import com.al.lunchapp.entities.MessageBean;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SocketController {
+
+    @MessageMapping("/user-all")
+    @SendTo("/topic/user")
+    public MessageBean send(@Payload MessageBean message) {
+        return message;
+    }
+}
